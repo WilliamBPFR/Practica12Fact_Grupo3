10dfc4dd3a104f4c40fc934dc84cf207564f4f8d
Rosanna Bautista <rosannaminyety@gmail.com>
2023-05-25 17:55:04 -0400

    Factor #4: Backing Services - Realizado

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
 const mongooseClient = require("mongoose");
 const config = require("./config.json");
-mongooseClient.connect(
-  config.databaseURL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    if (err) console.log(err);
-  }
-);
+mongooseClient.connect(config.databaseURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conexión exitosa a la base de datos');
+    // Resto de tu código aquí
+  })
+  .catch(err => {
+    console.error('Error al conectar a la base de datos:', err);
+  });
 
 const NotesSchema = mongooseClient.Schema({
   title: String,
